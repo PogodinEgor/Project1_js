db2e7aaefab9c39851fb301a5d7371101b4c62eb
Egor Pogodin <egor.pogodin@omega-r.com>
2024-10-01 18:01:15 +0300

    First task

---
+++ scripts.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const numberOfFilms = +prompt("Сколько фильмов вы просмотрели?", "1");
+
+const personalMoviesDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+	b = prompt("На сколько оцените его?", ""),
+	c = prompt("Один из последних просмотренных фильмов?", ""),
+	d = prompt("На сколько оцените его?", "");
+
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+console.log(personalMoviesDB)
